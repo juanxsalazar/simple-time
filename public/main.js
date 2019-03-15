737309fd59927b0304215117b4582e4d9e3f213a
Juan <juanxsalazar21@gmail.com>
2019-03-15 10:51:53 -0400

    cleaned up javascript code

--- public/main.js
+++ public/main.js
@@ -1,26 +1,23 @@
-let myHour=document.getElementById('hour');
-let myMinutes=document.getElementById('minutes');
-let mySecond=document.getElementById('second');
-let presentDate=document.getElementById('day');
+let myHour = document.getElementById('hour')
+let myMinutes = document.getElementById('minutes')
+let mySecond = document.getElementById('second')
+let presentDate = document.getElementById('day')
 
 const myClock = () => {
-  let now = new Date() ; 
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-  let second = now.getSeconds();
+  let now = new Date()
+  let hour = now.getHours()
+  let minutes = now.getMinutes()
+  let second = now.getSeconds()
 
-  if(second<10) {second='0'+second}
-  if(minutes<10){minutes='0'+minutes}
-  if(hour<10)   {hour='0'+hour}
-  
-  let currentDate = now.toDateString();
+  if (second < 10) { second = '0' + second }
+  if (minutes < 10) { minutes = '0' + minutes }
+  if (hour < 10) { hour = '0' + hour }
+  let currentDate = now.toDateString()
 
-presentDate.textContent = currentDate;
-myHour.textContent=hour + ':' ; 
-  myMinutes.textContent=minutes +':';
-  mySecond.textContent=second ;
+  presentDate.textContent = currentDate
+  myHour.textContent = hour + ':'
+  myMinutes.textContent = minutes + ':'
+  mySecond.textContent = second
 }
 
-setInterval(myClock, 500);
-
-document.addEventListener('DOMContentLoaded', main)
+setInterval(myClock, 500)
