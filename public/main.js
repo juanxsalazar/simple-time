beeca18ad1d3d204fe4e0be5716852956e86fdd2
Juan <juanxsalazar21@gmail.com>
2019-03-15 10:42:59 -0400

    added present date and time javascript. Needs am and pm

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let myHour=document.getElementById('hour');
+let myMinutes=document.getElementById('minutes');
+let mySecond=document.getElementById('second');
+let presentDate=document.getElementById('day');
+
+const myClock = () => {
+  let now = new Date() ; 
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  let second = now.getSeconds();
+
+  if(second<10) {second='0'+second}
+  if(minutes<10){minutes='0'+minutes}
+  if(hour<10)   {hour='0'+hour}
+  
+  let currentDate = now.toDateString();
+
+presentDate.textContent = currentDate;
+myHour.textContent=hour + ':' ; 
+  myMinutes.textContent=minutes +':';
+  mySecond.textContent=second ;
 }
+
+setInterval(myClock, 500);
 
 document.addEventListener('DOMContentLoaded', main)
